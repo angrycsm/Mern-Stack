08ae91fb9838e2fb1e7040aa3d3f6c7e2fd1ac9b
biellofy <gabrielmoraes12334@gmail.com>
2023-08-12 09:33:30 -0300

    refactor: adding software architecture

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 const express = require('express')
+const userRoute = require('./src/routes/user.route')
 const app = express()
 
-//ROTA
-    //Método HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
-        // GET - Pega uma informação
-        // POST - Cria uma informação
-        // PUT - Altera todas as informações
-        // PATH - Altera partes da informações
-        // DELETE - Apaga uma informação
+app.use('/soma', userRoute)
 
-    //Nome - Um identificador da minha 
-    
-    //Função(Callback) - Responsavel por executar algum comando
-
-    // res - responder
-
-    // json - ver arquivos json usando res.json()
-
-
-app.get("/soma", (req, res) => {
-  const soma = 1 + 1;
-
-  res.send({soma: soma});
-});
 
 app.listen(2411);
