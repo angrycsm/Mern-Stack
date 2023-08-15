a322ee6c22e261b4c8d4b331ac38ed96f8a192d5
biellofy <gabrielmoraes12334@gmail.com>
2023-08-14 21:24:29 -0300

    refactor: type modules, import adds..

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,56 +1,82 @@
-const userService = require('../services/user.service')
-const mongoose = require('mongoose')
+import userService from '../services/user.service.js'
 
 const create = async (req, res) => {
-  const { name, username, email, password, avatar, background } = req.body
+  try {
+    const { name, username, email, password, avatar, background } = req.body
 
-  if (!name || !username || !email || !password || !avatar || !background) {
-    res.status(400).send({ message: 'Submit all fields for registration' })
-  }
-
-  const user = await userService.createService(req.body)
-
-  if (!user) {
-    return res.status(400).send({ message: 'Error creating User' })
-  }
-
-  res.status(201).send({
-    message: 'User created successfully',
-    user: {
-      id: user._id,
-      name,
-      username,
-      email,
-      avatar,
-      background
+    if (!name || !username || !email || !password || !avatar || !background) {
+      res.status(400).send({ message: 'Submit all fields for registration' })
     }
-  })
+
+    const user = await userService.createService(req.body)
+
+    if (!user) {
+      return res.status(400).send({ message: 'Error creating User' })
+    }
+
+    res.status(201).send({
+      message: 'User created successfully',
+      user: {
+        id: user._id,
+        name,
+        username,
+        email,
+        avatar,
+        background
+      }
+    })
+  } catch (err) {
+    res.status(500).send({ message: err.message })
+  }
 }
 
 const findAll = async (req, res) => {
-  const users = await userService.findAllService()
+  try {
+    const users = await userService.findAllService()
 
-  if (users.length === 0) {
-    return res.status(400).send({ message: 'There are no registered users' })
+    if (users.length === 0) {
+      return res.status(400).send({ message: 'There are no registered users' })
+    }
+
+    res.send(users)
+  } catch (err) {
+    res.status(500).send({ message: err.message })
   }
-
-  res.send(users)
 }
 
 const findById = async (req, res) => {
-  const id = req.params.id
-
-  if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(400).send({ message: 'Invaid Id' })
+  try {
+    const { id, user } = req
+    res.send(user)
+  } catch (err) {
+    res.status(500).send({ message: err.message })
   }
-
-  const user = await userService.findByIdService(id)
-
-  if (!user) {
-    return res.status(400).send({ message: 'User not found' })
-  }
-
-  res.send(user)
 }
 
-module.exports = { create, findAll, findById }
+const update = async (req, res) => {
+  try {
+    const { name, username, email, password, avatar, background } = req.body
+
+    if (!name && !username && !email && !password && !avatar && !background) {
+      res.status(400).send({ message: 'Submit at least one field for update' })
+    }
+
+    const { id, user } = req
+
+    await userService.updateService(
+      id,
+      name,
+      username,
+      email,
+      password,
+      avatar,
+      background
+    )
+
+    res.send({ message: 'User successfully updated!' })
+  } catch (err) {
+    res.status(500).send({ message: err.message })
+  }
+}
+
+export default { create, findAll, findById, update };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,12 @@
-const userController = require("../controllers/user.controller");
+import express from "express"
+import userController from "../controllers/user.controller.js"
+import { validId, validUser } from '../middlewares/global.middlewares.js'
 
-const route = require("express").Router();
+const route = express.Router();
 
 route.post('/', userController.create);
 route.get('/', userController.findAll);
-route.get('/:id', userController.findById);
+route.get('/:id', validId, validUser, userController.findById);
+route.patch('/:id', validId, validUser, userController.update);
 
-module.exports = route
+export default route
